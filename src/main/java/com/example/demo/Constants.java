379d2dd79b129992eba50f9260f2995862232cc3
Daria <93216326+DariaaaK@users.noreply.github.com>
2021-11-11 21:47:32 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/Constants.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Constants {
+    public static final String email = "koltsovadasha2002@gmail.com";
+    public static final String birth = "26.09.2002";
+    public static final String name = "Daria";
+
+    public static void main(String[] args) {
+        System.out.println("Name    : " + name);
+        System.out.println("Birthday: " + birth);
+        System.out.println("E-mail  : " + email);
+    }
+}
